0aa05210c0d47300bc36efb1ca678ae363e1d34f
Piotr Kozlowski <piotr.kozlowski@itcrowd.pl>
2014-03-24 16:05:18 +0100

    Customize

--- window-dialog-commands.js
+++ window-dialog-commands.js
@@ -7,53 +7,111 @@
 'use strict';
 
 
+var isCommonJS = typeof window == "undefined" && typeof exports == "object";
+var alertThrows = function ()
+{
+    window.alert = function (msg)
+    {
+        throw new Error(msg);
+    };
+};
+var confirmThrows = function ()
+{
+    window.confirm = function (msg)
+    {
+        throw new Error(msg);
+    };
+};
+var promptThrows = function ()
+{
+    window.prompt = function (msg)
+    {
+        throw new Error(msg);
+    };
+};
 /**
  * Usage: alertOK() sets window.alert to return true when it is called in your application
  */
-angular.scenario.dsl('alertOK', function() {
-    return function() {
-        return this.addFutureAction('monkey patch window.alert to return true', function($window, $document, done) {
-            $window.alert = function() {return true;}
-            done();
-        });
-    };
-});
+jasmine.Env.prototype.alertOK = function (expectedMessage)
+{
+    window.alert = function (msg)
+    {
+        if (undefined != expectedMessage && msg != expectedMessage) {
+            throw new Error("Expected '" + expectedMessage + "' but got '" + msg + "'");
+        }
+        return true;
+    }
+};
+var alertOK = function (expectedMessage)
+{
+    return jasmine.getEnv().alertOK(expectedMessage);
+};
+if (isCommonJS) {
+    exports.alertOK = alertOK;
+}
 
 
 /**
  * Usage: confirmOK() sets window.confirm to return true when it is called in your application
  */
-angular.scenario.dsl('confirmOK', function() {
-    return function() {
-        return this.addFutureAction('monkey patch window.confirm to return true', function($window, $document, done) {
-            $window.confirm = function() {return true;}
-            done();
-        });
-    };
-});
+jasmine.Env.prototype.confirmOK = function (expectedMessage)
+{
+    window.confirm = function (msg)
+    {
+        if (undefined != expectedMessage && msg != expectedMessage) {
+            throw new Error("Expected '" + expectedMessage + "' but got '" + msg + "'");
+        }
+        return true;
+    }
+};
+var confirmOK = function (expectedMessage)
+{
+    return jasmine.getEnv().confirmOK(expectedMessage);
+};
+if (isCommonJS) {
+    exports.confirmOK = confirmOK;
+}
 
 
 /**
  * Usage: confirmCancel() sets window.confirm to return false when it is called in your application
  */
-angular.scenario.dsl('confirmCancel', function() {
-    return function() {
-        return this.addFutureAction('monkey patch window.confirm to return false', function($window, $document, done) {
-            $window.confirm = function() {return false;}
-            done();
-        });
-    };
-});
+jasmine.Env.prototype.confirmCancel = function (expectedMessage)
+{
+    window.confirm = function (msg)
+    {
+        if (undefined != expectedMessage && msg != expectedMessage) {
+            throw new Error("Expected '" + expectedMessage + "' but got '" + msg + "'");
+        }
+        return false;
+    }
+};
+var confirmCancel = function (expectedMessage)
+{
+    return jasmine.getEnv().confirmCancel(expectedMessage);
+};
+if (isCommonJS) {
+    exports.confirmCancel = confirmCancel;
+}
 
 
 /**
  * Usage: setPromptValue(value) sets window.prompt to return `value` when it is called in your application
  */
-angular.scenario.dsl('setPromptValue', function() {
-    return function(value) {
-        return this.addFutureAction('monkey patch window.prompt to return ' + value, function($window, $document, done) {
-            $window.prompt = function() {return value;}
-            done();
-        });
-    };
-});
+jasmine.Env.prototype.setPromptValue = function (value, expectedMessage)
+{
+    window.prompt = function (msg)
+    {
+        if (undefined != expectedMessage && msg != expectedMessage) {
+            throw new Error("Expected '" + expectedMessage + "' but got '" + msg + "'");
+        }
+        return value;
+    }
+};
+var setPromptValue = function (value, expectedMessage)
+{
+    return jasmine.getEnv().setPromptValue(value, expectedMessage);
+};
+if (isCommonJS) {
+    exports.setPromptValue = setPromptValue;
+}
